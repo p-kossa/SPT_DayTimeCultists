8030c1456e00814bbb3d2303386ca7ab835a9d9f
p-kossa <kossa526+git@gmail.com>
2024-07-16 08:22:49 -0400

    update for spt 390

--- Plugin.cs
+++ Plugin.cs
@@ -3,7 +3,8 @@ using BepInEx;
 
 namespace DayTimeCultists
 {
-    [BepInPlugin("com.DayTimeCultists", "DayTimeCultists", "1.1.0")]
+    [BepInPlugin("com.DayTimeCultists", "DayTimeCultists", "1.2.0")]
+    [BepInDependency("com.SPT.core", "3.9.0")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
